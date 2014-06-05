343a2ead30ce34dae9e05f1290219d8a0a8594d8
MigdaliaBrito <brito.m@gmercyu.edu>
2014-06-05 14:20:16 -0400

    Experiment

---
+++ javascript_fun.js
@@ -0,0 +1,8 @@
+var score;
+score = 3;
+
+//When declaring variables in Javascript, don't have to set variable type.
+var firstName = 'Peter';
+var lastName = 'Brito';
+var age = 21;
+var isAwesome = true
